c30af3429a3f1d297dc0d2cffecf9ed879ecc52c
Tin Le <thanhtin.le.vn@gmail.com>
2022-05-19 21:44:26 -0400

    exclude bin and obj

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,36 @@
+using System.Text;
+using System.Text.Json;
+using PlatformService.Dtos;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(plat),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("http://localhost:5069/api/c/platforms", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync POST to CommandService was OK!");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync POST to CommandService was NOT OK !");
+            }
+        }
+    }
+}
